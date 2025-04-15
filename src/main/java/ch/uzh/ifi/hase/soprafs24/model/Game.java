485b8e0dc8270aea14af42aeb9940f6525637972
Ahreum Oh <oharm4858@gmail.com>
2025-04-15 14:19:51 +0200

    update game model to deal with the situation that round is null

--- src/main/java/ch/uzh/ifi/hase/soprafs24/model/Game.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/model/Game.java
@@ -31,46 +31,81 @@ public class Game {
         this.ownerId = game.getOwner().getId();
         this.currentRoundStartPlayer = game.getStartPlayer();
         this.settings = new GameSettings(game.getSettings());
-        if (start) startRound();
+        if (start)
+            startRound();
     }
 
-    public int getCurrentRoundStartPlayer() {return currentRoundStartPlayer;}
-    public GameModel getGameModel(long userId){return new GameModel(this, userId);}
-    public List<Player> getPlayers() {return players;}
-    public long getOwnerId(){return ownerId;}
-    public long getSessionId(){return sessionId;}
-    public Round getRound(){return round;}
-    public RoundModel getRoundModel(long userId){return new RoundModel(round, userId);}
-    public GameSettings getSettings(){return settings;}
-    public void addPlayer(Player player){this.players.add(player);}
-    public Player removePlayer(long userId){return this.players.stream().filter(x -> x.getUserId() == userId).findFirst().orElse(null);}
-    public Player getPlayer(long userId){return players.stream().filter(x -> x.getUserId() == userId).findFirst().orElse(null);}
+    public int getCurrentRoundStartPlayer() {
+        return currentRoundStartPlayer;
+    }
 
-    public void startRound(){
+    public GameModel getGameModel(long userId) {
+        return new GameModel(this, userId);
+    }
+
+    public List<Player> getPlayers() {
+        return players;
+    }
+
+    public long getOwnerId() {
+        return ownerId;
+    }
+
+    public long getSessionId() {
+        return sessionId;
+    }
+
+    public Round getRound() {
+        return round;
+    }
+
+    public RoundModel getRoundModel(long userId) {
+        return new RoundModel(round, userId);
+    }
+
+    public GameSettings getSettings() {
+        return settings;
+    }
+
+    public void addPlayer(Player player) {
+        this.players.add(player);
+    }
+
+    public Player removePlayer(long userId) {
+        return this.players.stream().filter(x -> x.getUserId() == userId).findFirst().orElse(null);
+    }
+
+    public Player getPlayer(long userId) {
+        return players.stream().filter(x -> x.getUserId() == userId).findFirst().orElse(null);
+    }
+
+    public void startRound() {
         List<Player> livePlayers = getOnlinePlayers();
-        if(livePlayers.size() < 2) throw new ResponseStatusException(HttpStatus.CONFLICT, "at least two players required to start a round");
+        if (livePlayers.size() < 2)
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "at least two players required to start a round");
         this.round = new Round(livePlayers, this.currentRoundStartPlayer, this.settings);
     }
 
-    private List<Player> getOnlinePlayers(){
+    private List<Player> getOnlinePlayers() {
         return new ArrayList<>(players.stream().filter(Player::isOnline).toList());
     }
 
-    public boolean containsUser(long userId){
+    public boolean containsUser(long userId) {
         return this.players.stream().map(Player::getUserId).anyMatch(x -> x == userId);
     }
 
-    public void roundComplete(){
+    public void roundComplete() {
         this.round = null;
     }
 
-    public void joinSession(long userId){
+    public void joinSession(long userId) {
         Player player = getPlayer(userId);
-        if(player == null)return;
+        if (player == null)
+            return;
         player.setIsOnline(true);
     }
 
-    public void adjustSettings(GameSettings gameSettings){
+    public void adjustSettings(GameSettings gameSettings) {
         this.settings = gameSettings;
     }
 }
