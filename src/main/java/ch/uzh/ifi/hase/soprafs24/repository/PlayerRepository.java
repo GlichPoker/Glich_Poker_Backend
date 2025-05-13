12de76e350e3b2c465de58ce2096b0ae05f1c833
RealBlueSwan <nobrunner@hotmail.com>
2025-05-13 17:17:28 +0200

    potential fix for lobby join on localmachine

--- src/main/java/ch/uzh/ifi/hase/soprafs24/repository/PlayerRepository.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/repository/PlayerRepository.java
@@ -3,12 +3,13 @@ package ch.uzh.ifi.hase.soprafs24.repository;
 import ch.uzh.ifi.hase.soprafs24.entity.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface PlayerRepository extends JpaRepository<Player, Long> {
     @Query("SELECT p from Player p where p.user.id = :userId and p.game.id = :gameId")
-    Optional<Player> findByUserAndGameId(long userId, long gameId);
+    Optional<Player> findByUserAndGameId(@Param("userId") long userId, @Param("gameId") long gameId);
     List<Player> findByGameId(long gameId);
 }
